1c41ccfdbf653812c773d84487db23e60da29eec
Eran Withana <eran.withana@workday.com>
2014-07-08 15:33:11 -0700

    updating the copyright a bit

--- src/main/java/org/chinthaka/dropwizard/jdbi/dao/UserDAO.java
+++ src/main/java/org/chinthaka/dropwizard/jdbi/dao/UserDAO.java
@@ -1,5 +1,5 @@
 /*
- * Copyright [2014] [Eran C. Withana]
+ * Copyright 2014 Eran C. Withana
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
